3992d4f08fdea80fd2579bcf578a8fd69a0b5e2c
Zorian Zaiets <bravisabright@gmail.com>
2024-09-16 12:14:46 +0300

    Added message transfer to gmail

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const nodemailer = require('nodemailer');
 const port = process.env.PORT || 5000;
 const crypto = require('crypto');
 
@@ -29,8 +30,52 @@ app.post('/api/encrypt', (req, res) => {
     res.json({signature});
 });
 
+// Настройка транспортера для отправки почты через Gmail
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'bravisabright@gmail.com', // Ваша почта
+        pass: 'hxqk nbfx vmqs uqir',  // Пароль или специальный "App Password" для Gmail
+    },
+});
+
+app.post('/send-message', (req, res) => {
+    const { name, email, message } = req.body;
+
+    if (!name || !email || !message) {
+        return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    const mailOptions = {
+        from: email, // Адрес отправителя (тот, который ввел пользователь)
+        to: 'bravisabright@gmail.com', // Адрес, на который придет письмо
+        subject: `Message from ${name} (${email})`, // Тема письма
+        text: message, // Тело письма
+    };
+
+    // Здесь можно обработать данные (например, отправить на почту или сохранить в базу данных)
+    console.log(`Received message from ${name} (${email}): ${message}`);
+    // Отправляем письмо
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            return res.status(500).json({ error: 'Failed to send email' });
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.status(200).json({ message: 'Server: Message sent successfully!' });
+        }
+    });
+
+
+
+});
+
 app.get('/', (req, res) => {
     res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
 
 module.exports = app;
